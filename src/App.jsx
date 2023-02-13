439ce6bda92c3185474d80238e2154800d73377a
joker69216 <hamaraad883@gmail.com>
2023-02-13 11:56:13 +0300

    add animation to the scrolls

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Canvas } from '@react-three/fiber'
+import Box from './components/Box'
+
+export default function App() {
+  return (
+    <Canvas camera={{ position: [0, 0, 2] }}>
+      <Box position={[-0.75, 0, 0]} name="A" />
+      <Box position={[0.75, 0, 0]} name="B" />
+    </Canvas>
+  )
+}
---
+++ src/components/Box.jsx
@@ -0,0 +1,18 @@
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+export default function Box(props) {
+  const ref = useRef()
+
+  useFrame((_, delta) => {
+    ref.current.rotation.x += 1 * delta
+    ref.current.rotation.y += 0.5 * delta
+  })
+
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry />
+      <meshBasicMaterial color={0x00ff00} wireframe />
+    </mesh>
+  )
+}
