41c48c73c88345c604bc52996d3fe6572cc34424
joker69216 <hamaraad883@gmail.com>
2023-02-13 10:57:08 +0300

    add needed libraries / delete uneeded files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import './styles.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Canvas camera={{ position: [0, 0, 2] }}>
+      <mesh>
+        <boxGeometry />
+        <meshBasicMaterial color={0x00ff00} wireframe />
+      </mesh>
+    </Canvas>
+  </StrictMode>
+)
