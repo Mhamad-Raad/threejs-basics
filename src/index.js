af4d47022574d825de23c784b2f960579caecd87
joker69216 <hamaraad883@gmail.com>
2023-02-13 10:58:28 +0300

    fix minor bug with css import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import './styles.css'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
