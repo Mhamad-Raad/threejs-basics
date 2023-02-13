e91c08a6a89e6566fcb58b14126dd24cd16d3387
joker69216 <hamaraad883@gmail.com>
2023-02-13 14:46:23 +0300

    add leva and lights

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,146 @@ import { Canvas } from '@react-three/fiber'
 import Polyhedron from './components/Polyhedron'
 import * as THREE from 'three'
 import { Stats, OrbitControls } from '@react-three/drei'
+import { useControls } from 'leva'
+import { useRef } from 'react'
 
-export default function App() {
-  const polyhedron = [
-    new THREE.BoxGeometry(),
-    new THREE.SphereGeometry(0.785398),
-    new THREE.DodecahedronGeometry(0.785398)
-  ]
+function Lights() {
+  const ambientRef = useRef()
+  const directionalRef = useRef()
+  const pointRef = useRef()
+  const spotRef = useRef()
+
+  useControls('Ambient Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        ambientRef.current.visible = v
+      }
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        ambientRef.current.color = new THREE.Color(v)
+      }
+    }
+  })
+
+  useControls('Directional Light', {
+    visible: {
+      value: true,
+      onChange: (v) => {
+        directionalRef.current.visible = v
+      }
+    },
+    position: {
+      x: 1,
+      y: 1,
+      z: 1,
+      onChange: (v) => {
+        directionalRef.current.position.copy(v)
+      }
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        directionalRef.current.color = new THREE.Color(v)
+      }
+    }
+  })
+
+  useControls('Point Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        pointRef.current.visible = v
+      }
+    },
+    position: {
+      x: 2,
+      y: 0,
+      z: 0,
+      onChange: (v) => {
+        pointRef.current.position.copy(v)
+      }
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        pointRef.current.color = new THREE.Color(v)
+      }
+    }
+  })
+
+  useControls('Spot Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        spotRef.current.visible = v
+      }
+    },
+    position: {
+      x: 3,
+      y: 2.5,
+      z: 1,
+      onChange: (v) => {
+        spotRef.current.position.copy(v)
+      }
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        spotRef.current.color = new THREE.Color(v)
+      }
+    }
+  })
 
   return (
-    <Canvas camera={{ position: [0, 0, 3] }}>
-      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+    <>
+      <ambientLight ref={ambientRef} />
+      <directionalLight ref={directionalRef} />
+      <pointLight ref={pointRef} />
+      <spotLight ref={spotRef} />
+    </>
+  )
+}
+
+export default function App() {
+  return (
+    <Canvas camera={{ position: [4, 4, 1.5] }}>
+      <Lights />
+      <Polyhedron
+        name="meshBasicMaterial"
+        position={[-3, 1, 0]}
+        material={
+          new THREE.MeshBasicMaterial({ color: 'yellow', flatShading: true })
+        }
+      />
+      <Polyhedron
+        name="meshNormalMaterial"
+        position={[-1, 1, 0]}
+        material={new THREE.MeshNormalMaterial({ flatShading: true })}
+      />
+      <Polyhedron
+        name="meshPhongMaterial"
+        position={[1, 1, 0]}
+        material={
+          new THREE.MeshPhongMaterial({ color: 'lime', flatShading: true })
+        }
+      />
+      <Polyhedron
+        name="meshStandardMaterial"
+        position={[3, 1, 0]}
+        material={
+          new THREE.MeshStandardMaterial({
+            color: 0xff0033,
+            flatShading: true
+          })
+        }
+      />
+      <OrbitControls target={[2, 2, 0]} />
+      <axesHelper args={[5]} />
+      <gridHelper />
       <Stats />
-      <OrbitControls />
     </Canvas>
   )
 }
--- src/components/Polyhedron.jsx
+++ src/components/Polyhedron.jsx
@@ -1,26 +1,17 @@
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 
-export default function Polyhedron({ position, polyhedron }) {
+export default function Polyhedron(props) {
   const ref = useRef()
-  const [count, setCount] = useState(0)
-
-  console.log(polyhedron)
 
   useFrame((_, delta) => {
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += 0.5 * delta
+    ref.current.rotation.x += 0.2 * delta
+    ref.current.rotation.y += 0.05 * delta
   })
 
   return (
-    <mesh
-      position={position}
-      ref={ref}
-      onPointerDown={() => {
-        setCount((count + 1) % 3)
-      }}
-      geometry={polyhedron[count]}>
-      <meshBasicMaterial color={'lime'} wireframe />
+    <mesh {...props} ref={ref}>
+      <icosahedronGeometry args={[1, 1]} />
     </mesh>
   )
 }
