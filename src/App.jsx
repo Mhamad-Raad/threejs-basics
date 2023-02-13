ba0569b701ca1e7da0e0213958dea0bcc1ddf3b0
joker69216 <hamaraad883@gmail.com>
2023-02-13 13:10:08 +0300

    add object for memory reusability

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Canvas } from '@react-three/fiber'
-import Box from './components/Box'
+import Polyhedron from './components/Polyhedron'
+import * as THREE from 'three'
 
 export default function App() {
+  const polyhedron = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398)
+  ]
+
   return (
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box position={[-0.75, 0, 0]} name="A" />
-      <Box position={[0.75, 0, 0]} name="B" />
+    <Canvas camera={{ position: [0, 0, 3] }}>
+      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
     </Canvas>
   )
 }
